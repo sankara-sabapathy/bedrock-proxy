cea1f6d92a1572fe2bf6e0dc9762f7d004a3e960
Sankara Sabapathy <sankara.sabapathy@aircanada.ca>
2023-10-14 08:50:04 +0530

    response streaming minimal.

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 'use strict';
-
-// const { BedrockRuntime } = require('aws-sdk')
+const readline = require('readline');
 const { BedrockRuntimeClient, InvokeModelWithResponseStreamCommand } = require("@aws-sdk/client-bedrock-runtime");
-const client= new BedrockRuntimeClient({apiVersion: '2023-09-30', region: 'us-west-2' , defaultsMode: 'legacy' });
+const client = new BedrockRuntimeClient({ apiVersion: '2023-09-30', region: 'us-west-2' });
 
-module.exports.handler = awslambda.streamifyResponse(async (event, responseStream) => {
-  const input = JSON.parse(event.body);
-  if(!input.prompt) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: "Missing prompt / Prompt length greater than 50 char"
-      }, null, 2)
-    }
-  }
-  responseStream.setContentType('text/plain');
-  responseStream.write('Hello, world!');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  keepOpen: true,
+});
 
-
+rl.question('Type in your prompt : ', async (prompt) => {
   const params = {
     modelId: "cohere.command-text-v14",
     contentType: "application/json",
     accept: "*/*",
     body: JSON.stringify({
-      prompt: input.prompt,
+      prompt: prompt,
       max_tokens: 10,
       temperature: 0.75,
       p: 0.01,
@@ -31,35 +23,21 @@ module.exports.handler = awslambda.streamifyResponse(async (event, responseStrea
       stop_sequences: [],
       return_likelihoods: 'NONE',
       stream: true
-    })     
+    })
   }
-
   const command = new InvokeModelWithResponseStreamCommand(params);
+
   const response = await client.send(command);
+
   const chunks = [];
   for await (const chunk of response.body) {
-    console.log(JSON.parse(Buffer.from(chunk.chunk.bytes).toString('utf-8')).text)
-    responseStream.write(JSON.parse(Buffer.from(chunk.chunk.bytes).toString('utf-8')).text);
-    chunks.push(JSON.parse(Buffer.from(chunk.chunk.bytes).toString('utf-8')).text);
+    const parsedChunk = JSON.parse(Buffer.from(chunk.chunk.bytes).toString('utf-8'))
+    if (parsedChunk.is_finished || parsedChunk.text === '<EOS_TOKEN>') {
+      continue;
+    }
+    process.stdout.write(parsedChunk.text);
+    chunks.push(parsedChunk.text);
   }
-  console.log(chunks);
-  console.log(JSON.stringify(chunks))
-
-  responseStream.end();
-
-  // const responseObject = JSON.parse(Buffer.from(response.body).toString());
-  // const responseText = responseObject.completions[0]?.data?.text;
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     text: response
-  //   }, null, 2)
-  // };
+  
+  rl.close();
 });
-
-
-// handler({
-//   body: JSON.stringify({
-//     prompt: "What is the capital of France?"
-//   })})
-
